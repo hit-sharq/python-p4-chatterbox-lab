9f2971a9dd514c2f84a9d7f9c2305d97b6c12151
joshua mwendwa <leeroyjoshu087@gmail.com>
2024-10-08 21:35:00 +0300

    Added

--- server/app.py
+++ server/app.py
@@ -14,13 +14,40 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'POST':
+        message = Message(
+            body=request.json['body'],
+            username=request.json['username'],
+        )
+        db.session.add(message)
+        db.session.commit()
+        return jsonify(message.serialize), 201
 
-@app.route('/messages/<int:id>')
+    messages = Message.query.all()
+    return jsonify([message.serialize for message in messages])
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
 
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+            
+        db.session.add(message)
+        db.session.commit()
+
+        return make_response(message.to_dict(),200 )
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        return make_response( {'deleted': True} , 200)
+    
 if __name__ == '__main__':
     app.run(port=5555)
